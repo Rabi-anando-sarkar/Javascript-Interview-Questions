172e84af1440e2f0d0f27fa421d9254dd2198d92
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-10-08 00:18:23 +0530

    Factorial: 1 methods

---
+++ codeEight.js
@@ -0,0 +1,15 @@
+
+const factorial = (num) => {
+    let fact = 1
+    for(let i=1; i <= num; i++) {
+        fact*=i
+    }
+    return fact
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
